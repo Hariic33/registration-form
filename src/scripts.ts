32b7c40ae1d85494ff831352f415a0e1119b4d88
Hariic33 <hmakbets@gmail.com>
2023-08-11 00:56:34 +0300

    implemented axios

--- src/scripts.ts
+++ src/scripts.ts
@@ -1,42 +1,63 @@
-function showRuncis(): void {
-    const registrationForm = document.querySelector(".registration-form") as HTMLElement;
-    const runcisImage = document.getElementById("Runcis") as HTMLElement;
-  
-    registrationForm.style.display = "none";
-    runcisImage.style.display = "block";
+import axios from 'axios';
+
+function validateForm(): boolean {
+  const requiredFields: string[] = ["first_name", "last_name", "email", "password", "country"];
+  const genderMale: boolean = (document.getElementById("male") as HTMLInputElement).checked;
+  const genderFemale: boolean = (document.getElementById("female") as HTMLInputElement).checked;
+  const termsChecked: boolean = (document.getElementById("terms") as HTMLInputElement).checked;
+
+  for (const field of requiredFields) {
+    const inputField = document.getElementById(field) as HTMLInputElement;
+    if (!inputField.value.trim()) {
+      alert("Please fill out all required fields.");
+      return false;
+    }
   }
-  
-  document.addEventListener("keypress", function (event: KeyboardEvent): void {
-    if (event.keyCode === 13) {
-      event.preventDefault();
-      if (validateForm()) {
-        showRuncis();
-      }
+
+  if (!genderMale && !genderFemale) {
+    alert("Please select a gender.");
+    return false;
+  }
+
+  if (!termsChecked) {
+    alert("Please agree to the terms and conditions.");
+    return false;
+  }
+
+  return true;
+}
+
+const form = document.getElementById('registrationForm') as HTMLFormElement;
+
+form.addEventListener('submit', function (event) {
+  event.preventDefault();
+
+  const formData = new FormData(form);
+
+  const data: { [key: string]: any } = {};
+  formData.forEach((value, key) => {
+    if (key === "password") {
+      data[key] = "********";
+    } else {
+      data[key] = value;
     }
   });
-  
-  function validateForm(): boolean {
-    const requiredFields: string[] = ["first_name", "last_name", "email", "password", "country"];
-    const genderMale: boolean = (document.getElementById("male") as HTMLInputElement).checked;
-    const genderFemale: boolean = (document.getElementById("female") as HTMLInputElement).checked;
-    const termsChecked: boolean = (document.getElementById("terms") as HTMLInputElement).checked;
-    for (const field of requiredFields) {
-      const inputField = document.getElementById(field) as HTMLInputElement;
-      if (!inputField.value.trim()) {
-        alert("Please fill out all required fields.");
-        return false;
-      }
+
+  axios.post('http://localhost:3004/posts', data)
+    .then(response => {
+      console.log('Form data submitted successfully!', response);
+    })
+    .catch(error => {
+      console.error('Error submitting form data:', error);
+    });
+});
+
+document.addEventListener("keypress", function (event: KeyboardEvent): void {
+  if (event.key === "Enter") {
+    event.preventDefault();
+    if (validateForm()) {
+      const submitEvent = new Event("submit", { bubbles: true, cancelable: true });
+      form.dispatchEvent(submitEvent);
     }
-  
-    if (!genderMale && !genderFemale) {
-      alert("Please select a gender.");
-      return false;
-    }
-  
-    if (!termsChecked) {
-      alert("Please agree to the terms and conditions.");
-      return false;
-    }
-  
-    return true;
   }
+});
